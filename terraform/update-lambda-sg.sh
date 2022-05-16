8aef74bbb08071c44e28bebdc878f4d09db660f0
Peter Taylor <petert@Thunder.local>
2022-05-16 14:09:11 +0100

    Updating script to create a temporarySG and assign ENI's to that before removing the temporary SG

--- terraform/update-lambda-sg.sh
+++ terraform/update-lambda-sg.sh
@@ -6,25 +6,23 @@ VPC_ID=$1
 # source security group that we will remove from enis
 SOURCE_SG_ID=$2
 
-# optional - target security group that we will move the enis to, if not specified use default sg
-TARGET_SG_ID=$3
+# create Temporary SG
+tempSG=$(aws ec2 create-security-group --description "Temp SG" --group-name "Temp-SG" | jq -r '.GroupId')
 
-if [ -z $TARGET_SG_ID ]; then
-    TARGET_SG_ID=$(aws ec2 describe-security-groups \
-		     --filters Name=description,Values='default VPC security group' \
-		     Name=vpc-id,Values=${VPC_ID} \
-		     --output text \
-		     --query 'SecurityGroups[0].GroupId')
-fi
-
+# Get ENI's from source SG
 enis=$(aws ec2 describe-network-interfaces \
 	   --filters Name=group-id,Values=${SOURCE_SG_ID} Name=interface-type,Values=lambda \
 	   --output text \
 	   --query 'NetworkInterfaces[*].NetworkInterfaceId')
 
-# Change security groups on the lambda nics
+# Assign eni's to temporary SG
 for item in ${enis}; do
-  aws ec2 modify-network-interface-attribute --network-interface-id ${item} --groups ${TARGET_SG_ID}
+  aws ec2 modify-network-interface-attribute --network-interface-id ${item} --groups ${tempSG}
 done
 
-echo detached ${enis} from ${SOURCE_SG_ID} to ${TARGET_SG_ID}
+echo detached ${enis} from ${SOURCE_SG_ID} to ${tempSG}
+
+# Delete Temporary SG
+echo "Deleting temporary SG ${tempSG}"
+
+aws ec2 delete-security-group --group-id ${tempSG}
