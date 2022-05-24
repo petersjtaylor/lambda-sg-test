4364f390bd9ff76a78c170a72bc662093b2c0632
Peter Taylor <petert@kainos.com>
2022-05-24 08:16:28 +0000

    Updates

--- terraform/tests/infra_tests.sh
+++ terraform/tests/infra_tests.sh
@@ -5,9 +5,6 @@ validate_efs_mount() {
   ssh -i ../node.pem ec2-user@$(cat ../publicIP.txt) mountpoint -q /wibble
 }
 test_validate_efs_mount() {
-  fake mount 'echo $FAKE_PARAMS >/tmp/fake_params'
-
-
   assert_status_code 0 validate_efs_mount
 }
 
