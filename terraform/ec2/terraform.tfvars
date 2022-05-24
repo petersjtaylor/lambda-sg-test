a30da610a25d6472af2b0bf7cea571804a07a24a
Peter Taylor <peter.taylor1@landregistry.gov.uk>
2022-05-24 14:25:17 +0100

    Removing unused vars

--- terraform/ec2/terraform.tfvars
+++ terraform/ec2/terraform.tfvars
@@ -1,4 +1,1 @@
-subnet_public_cidr_block = "10.0.0.0/21"
-subnet_private_cidr_block = "10.0.8.0/21"
-vpc_cidr_block = "10.0.0.0/16"
 project = "lambda-cleanup"
--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -7,18 +7,3 @@ variable "region" {
   type        = string
   default     = "eu-west-2"
 }
-
-variable "vpc_cidr_block" {
-  type        = string
-  description = "VPC CIDR"
-}
-
-variable "subnet_public_cidr_block" {
-  type        = string
-  description = "Public subnet CIDR"
-}
-
-variable "subnet_private_cidr_block" {
-  type        = string
-  description = "Private subnet CIDR"
-}
