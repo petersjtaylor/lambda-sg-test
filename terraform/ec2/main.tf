5b61f816a857e06ee4f5eb98d450487961e13062
Peter Taylor <peter.taylor1@landregistry.gov.uk>
2022-05-25 11:40:28 +0100

    Adding teardown for cleanup

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -182,8 +182,8 @@ resource "null_resource" "configure_nfs" {
   provisioner "remote-exec" {
     inline = [
     "sudo mkdir -p /wibble",
-    "sudo chmod go+rw /wibble",
-    "mountpoint -q /wibble && echo 'Nothing to do' || sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,port=2049 ${aws_efs_mount_target.mount.ip_address}:/ /wibble"
+    "mountpoint -q /wibble && echo 'Nothing to do' || sudo mount -t nfs -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,port=2049 ${aws_efs_mount_target.mount.ip_address}:/ /wibble",
+    "sudo chown ec2-user:ec2-user /wibble"
     ]
   }
 }
--- terraform/tests/infra_tests.sh
+++ terraform/tests/infra_tests.sh
@@ -4,10 +4,24 @@ lambda_call() {
 validate_efs_mount() {
   ssh -i ../ec2/node.pem ec2-user@$(cat ../ec2/publicIP.txt) mountpoint -q /wibble
 }
-test_validate_efs_mount() {
+
+create_file() {
+  ssh -i ../ec2/node.pem ec2-user@$(cat ../ec2/publicIP.txt) touch /wibble/file
+}
+test_efs_mount() {
   assert_status_code 0 validate_efs_mount
 }
 
 test_lambda_is_accessible() {
   assert_status_code 0 lambda_call
+}
+
+test_efs_mount_is_writeable() {
+  create_file
+
+  assert "ssh -i ../ec2/node.pem ec2-user@$(cat ../ec2/publicIP.txt) test -e /wibble/file"
+}
+
+teardown_suite() {
+  ssh -i ../ec2/node.pem ec2-user@$(cat ../ec2/publicIP.txt) rm /wibble/file
 }
